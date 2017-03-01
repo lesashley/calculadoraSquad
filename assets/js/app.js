06dfab6cf61240edd64e9defadc3c20261a98c24
Maribel <msevilla318@gmail.com>
2017-03-01 12:34:22 -0500

    segundo cambio de la funcion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 var num1 = document.getElementById("num1").value;
 var num2 = document.getElementById("num2").value;
+var resultado = document.getElementById("resultado");
+
 function potencia(num1,num2) {
-  return Math.pow(num1,num2)
+  return resultado.innerHTML= Math.pow(num1,num2);
 }
