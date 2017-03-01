a61d8fb1a1f14d8be5300d1a179aee531bc73387
Luz Gutierrez <mili01gm@gmail.com>
2017-03-01 12:58:28 -0500

    Operaciones simples

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,42 @@
-var uno = document.getElementById('num1');
-var dos = document.getElementById('num2');
-var operacion = document.getElementById('operacion');
-var pregunta = document.getElementById('pregunta');
+function operar(){
+  var uno = document.getElementById('num1').value;
+  var dos = document.getElementById('num2').value;
+  var operacion = document.getElementById('operacion').value;
 
-function suma(a,b){
-  return a+b;
-}
+  function suma(a,b){
+    var a = parseInt(a);
+    var b = parseInt(b);
+    return a+b;
+  }
 
-function operar(ant,pos){
+  function resta(a,b){
+    var a = parseInt(a);
+    var b = parseInt(b);
+    return a-b;
+  }
+
+  function division(a,b){
+    var a = parseInt(a);
+    var b = parseInt(b);
+    return a/b;
+  }
+
+  function multiplicacion(a,b){
+    var a = parseInt(a);
+    var b = parseInt(b);
+    return a*b;
+  }
+
   var respuesta;
-    if(operacion.value == 1){ respuesta = suma(ant,pos);}
-    else if(operacion.value == 2){respuesta=resta(ant,pos);}
-    else if(operacion.value == 3){respuesta=multiplica(ant,pos);}
-    else if(operacion.value == 4){respuesta = division(ant,pos);}
+    if(operacion == 1){ respuesta = suma(uno,dos);}
+    else if(operacion == 2){respuesta=resta(uno,dos);}
+    else if(operacion == 3){respuesta=multiplica(unos,dos);}
+    else if(operacion == 4){respuesta = division(unos,dos);}
 
-  return respuesta;
+  document.getElementById('resultado').innerHTML = respuesta;
 }
 
-console.log(operar(uno.value,dos.value));
+
 
 /*
 if(pregunta.value == 1){
