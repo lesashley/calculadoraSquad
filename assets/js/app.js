188e04bc49247ae32b716ddd5d387b82877cb2a4
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-01 12:16:34 -0500

    sumav1

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function calcSuma(){
+  var num1=document.getElementById('num1').value;
+  var num2=document.getElementById('num2').value;
+
+  //result= num1 + num2;
+
+  document.getElementById("result").value=num1 +num2;
+}
