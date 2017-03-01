5bfcdcb0d28288e33b9e1750bf316bc1b10ee738
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-01 12:26:01 -0500

    funcion multiplicar

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+  var num1 = document.getElementById('id').value;
+  var num2 = document.getElementById('').value;
+  var resultado = document.getElementById();
+
+function multiplicar (){
+  return resultado.innerHTML= parseInt(num1) * parseInt(num2);
+}
