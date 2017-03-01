d182f13fb0e85e176c83885aca720d9355e2f92a
Miriam Peralta <miiriampc@gmail.com>
2017-03-01 12:02:34 -0500

    funcion division mas operacion

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+var num1=document.getElementById('id').value;
+var num2=document.getElementById('').value;
+var opcion=document.getElementById('id').value;
+
+var resultado=document.getElementById();
+
+function division(){
+  while(num2!=0){
+    return resultado.innerHTML=num1/num2;
+  }
+  return resultado.innerHTML="Valor indefinido";
+}
+
+function operacion(){
+  switch (expression) {
+    case 1: suma();break;
+    case 2: resta();break;
+    case 3: multiplicacion();break;
+    case 4: division();break;
+    case 5: potencia();break;
+    case 6: raiz();break;
+    default:
+  }
+}
