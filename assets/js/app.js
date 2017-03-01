ee285a7d0fb269ff90341728f09c64ee6a408385
Maribel <msevilla318@gmail.com>
2017-03-01 12:22:58 -0500

    arreglos de funcion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value;
 function potencia(num1,num2) {
-  Math.pow(num1,num2)
+  return Math.pow(num1,num2)
 }
